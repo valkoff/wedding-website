4111a1f687f91cd61bddbf112a5b36595c487edc
Valerio Cofano <valerio.cofano@camarasoft.com>
2025-01-13 12:12:18 +0100

    suggerimenti alloggi

--- components/Alloggio.vue
+++ components/Alloggio.vue
@@ -3,10 +3,7 @@
         <h1 class="text-2xl tracking-widest text-primary">{{ title }}</h1>
         <p class="text-lg font-bold">{{ subtitle }}</p>
         <p>{{ description }}</p>
-        <p class="font-italic">{{ elapsed_time }}</p>
-        <div class="text-xl font-bold text-primary">
-            <a :href="indication_link" target="_blank">Indicazioni</a>
-        </div>
+        <p class="italic">{{ elapsed_time }}</p>
     </div>
 
 </template>
@@ -18,6 +15,5 @@ defineProps({
     subtitle: String,
     description: String,
     elapsed_time: String,
-    indication_link: String
 })
 </script>
--- components/LocationView.vue
+++ components/LocationView.vue
@@ -33,17 +33,47 @@
             viaggio, in ordine di distanza dalla Location
         </p>
         <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
-            <Alloggio title="Roma" subtitle="La Capitale"
-                description="Se volete sfruttare l'occasione per visitarla, vi consigliamo di trattenervi qualche giorno in più. Inoltre, con il Giubileo potreste godere di eventi e manifestazioni speciali."
-                elapsed_time="> 1 ora in macchina" indication_link="https://maps.app.goo.gl/j8ezmynL2KT5zeQ99" />
+            <div>
+                <Alloggio title="Roma" subtitle="La Capitale"
+                    description="Se volete sfruttare l'occasione per visitarla, vi consigliamo di trattenervi qualche giorno in più. Inoltre, con il Giubileo potreste godere di eventi e manifestazioni speciali."
+                    elapsed_time="> 1 ora in macchina" />
+                <h6 class="mt-2 text-lg font-bold">Suggerimenti: </h6>
+                <div class="font-bold text-primary">
+                    <a href="https://www.booking.com/searchresults.it.html?label=gen173nr-1FCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQzYAQHoAQH4AQyIAgGoAgO4Auvlk7wGwAIB0gIkY2U0YzYwMGYtNTM5ZC00NTQ0LTg1NWYtNDQyMGUyOTBmOWIw2AIG4AIB&sid=9d4e5eb6029fb6208167a139f7880a82&aid=304142&ss=Roma%2C+Lazio%2C+Italia&ssne=Monterotondo&ssne_untouched=Monterotondo&lang=it&sr_view=grid&dest_id=-126693&dest_type=city&ac_position=1&ac_click_type=b&ac_langcode=it&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=14f84e61867d0296&checkin=2025-06-01&checkout=2025-06-02&group_adults=1&no_rooms=1&group_children=0&nflt=oos%3D1&order=bayesian_review_score"
+                        target="_blank">Cerca su Booking.com</a>
+                </div>
+            </div>
 
-            <Alloggio title="Monterotondo" subtitle="Comoda e servita" description="Alle porte di Roma, facilmente raggiungibile in treno. Una cittadina con tutti i servizi a
-                    disposizione." elapsed_time="circa 40 min in macchina"
-                indication_link="https://maps.app.goo.gl/j8ezmynL2KT5zeQ99" />
+            <div>
+                <Alloggio title="Monterotondo" subtitle="Comoda e servita" description="Alle porte di Roma, facilmente raggiungibile in treno. Una cittadina con tutti i servizi a
+                        disposizione." elapsed_time="circa 40 min in macchina"
+                    indication_link="https://maps.app.goo.gl/j8ezmynL2KT5zeQ99" />
 
-            <Alloggio title="Vicino la location" subtitle="Zero sbatti"
-                description="Se te pesa er culo, vuoi svegliarti in mezzo alle pecore e vuoi tornare strisciando dopo aver ingerito più alcool di un vichingo, è meglio non allontanarsi troppo."
-                elapsed_time="circa 5 min ciondolando" indication_link="https://maps.app.goo.gl/j8ezmynL2KT5zeQ99" />
+                <h6 class="mt-2 text-lg font-bold">Suggerimenti: </h6>
+                <div class="font-bold text-primary">
+                    <a href="https://www.booking.com/searchresults.it.html?label=gen173nr-1FCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQzYAQHoAQH4AQyIAgGoAgO4Auvlk7wGwAIB0gIkY2U0YzYwMGYtNTM5ZC00NTQ0LTg1NWYtNDQyMGUyOTBmOWIw2AIG4AIB&sid=9d4e5eb6029fb6208167a139f7880a82&aid=304142&ss=Monterotondo&ssne=Scandriglia&ssne_untouched=Scandriglia&lang=it&src=searchresults&sr_view=grid&checkin=2025-06-01&checkout=2025-06-02&group_adults=1&no_rooms=1&group_children=0"
+                        target="_blank">Cerca su Booking.com</a>
+                </div>
+            </div>
+
+            <div>
+
+                <Alloggio title="Vicino la location" subtitle="Zero sbatti"
+                    description="Se te pesa er culo, vuoi svegliarti in mezzo alle pecore e vuoi tornare strisciando dopo aver ingerito più alcool di un vichingo, è meglio non allontanarsi troppo."
+                    elapsed_time="circa 5 min ciondolando"
+                    indication_link="https://maps.app.goo.gl/j8ezmynL2KT5zeQ99" />
+
+                <h6 class="mt-2 text-lg font-bold">Suggerimenti: </h6>
+                <div class="font-bold text-primary">
+                    <a href="https://www.laripa.com" target="_blank">Agriturismo La Ripa</a> <br />
+                    <a href="https://www.facebook.com/profile.php?id=100063743431945" target="_blank">Il
+                        Viandante</a><br />
+                    <a href="https://www.booking.com/searchresults.it.html?label=gen173nr-1FCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQzYAQHoAQH4AQyIAgGoAgO4Auvlk7wGwAIB0gIkY2U0YzYwMGYtNTM5ZC00NTQ0LTg1NWYtNDQyMGUyOTBmOWIw2AIG4AIB&sid=9d4e5eb6029fb6208167a139f7880a82&aid=304142&checkin=2025-06-01&checkout=2025-06-02&dest_id=-129182&dest_type=city&srpvid=c8044de1d10d053b&order=distance_from_search&sr_view=grid#map_opened"
+                        target="_blank">Cerca su Booking.com</a>
+                </div>
+
+            </div>
+
         </div>
 
     </div>
