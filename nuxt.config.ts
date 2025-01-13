d43a7d40063efa7aee24baef2157223cb18b89a9
Valerio Cofano <valerio.cofano@camarasoft.com>
2025-01-13 12:33:42 +0100

    title, meta tags e favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,16 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  app: {
+    head: {
+      title: 'A&V 1.06.25',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Matrimonio Antonietta e Valerio, 1 Giugno 2025, La Seminatrice' },
+        { name: 'author', content: 'Valerio Cofano' },
+        { name: 'theme-color', content: '#a2bd95' },
+      ],
+    },
+  },
 })
